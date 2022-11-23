294fecdf7c1a68c1e7a3991d7c714666e2fe9df5
Aggron-S <114202121+Aggron-S@users.noreply.github.com>
2022-11-23 11:06:29 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function openNav() {
+  document.getElementById("websiteMenuToggle").style.width = "18.75rem";
+}
+function closeNav() {
+  document.getElementById("websiteMenuToggle").style.width = "0";
+}
